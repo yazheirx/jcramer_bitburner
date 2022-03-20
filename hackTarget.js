36002535d1b2b1b324e155b8d21a2d42e60b364b
Justin G. Cramer <jcramer@ludicrousintellect.com>
2022-03-19 20:19:35 -0500

    Improved documentation in hackTarget.js

--- hackTarget.js
+++ hackTarget.js
@@ -1,14 +1,21 @@
+// Can be run from the command line or called from another script
+// arha.js calls this script
+
 /** @param {NS} ns **/
 export async function main(ns) {
 	let target = ns.args[0];
+	// Check which hack programs exist
 	let ckBruteSSH = ns.fileExists("BruteSSH.exe");
 	let ckFTPCrack = ns.fileExists("FTPCrack.exe");
 	let ckrelaySMTP = ns.fileExists("relaySMTP.exe");
 	let ckHTTPWorm = ns.fileExists("HTTPWorm.exe");
 	let ckSQLInject = ns.fileExists("SQLInject.exe")
+	// restart hacked ports counter
 	let portsHacked = 0;
 	let numerOfPorts = ns.getServerNumPortsRequired(target);
+	// Don't hack what you already have root to
 	if (ns.hasRootAccess(target) != true) {
+		// start hacking ports
 		if (ckBruteSSH) {
 			ns.brutessh(target);
 			ns.print("Running BruteSSH on ", target);
@@ -34,6 +41,7 @@ export async function main(ns) {
 			ns.print("Running HTTSQLInjectPWorm on ", target);
 			portsHacked++;
 		}
+		// if you have hacked enough ports nuke the target.  Else, report failure and move on
 		if (numerOfPorts <= portsHacked) {
 			ns.nuke(target);
 		} else {
